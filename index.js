215fc23b4900454e2c4ddb326da733208c2e7848
Shashitha-Ashan <shashithaashan555@gmail.com>
2024-09-18 16:28:51 +0530

    deploy vercel

--- index.js
+++ index.js
@@ -73,5 +73,5 @@ app.get("/", (req, res) => {
 });
 
 app.listen(process.env.PORT || 3000, () =>
-  console.log(`Server listening on port ${process.env.PORT}`)
+  console.log(`Server listening on port`)
 );
