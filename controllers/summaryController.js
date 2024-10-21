eedf70ce46a07ede26d092aa8ce23f1e5e67732f
Shashitha-Ashan <shashithaashan555@gmail.com>
2024-10-21 14:56:30 +0530

    added lecture summary and admin summary about sessions

--- controllers/summaryController.js
+++ controllers/summaryController.js
@@ -1,64 +1,53 @@
 const TimeTableSlot = require("../models/timeTableSlotModel");
-
-const getLecturerSummary = async (lecturerId) => {
-  const summary = await TimeTableSlot.aggregate([
-    {
-      $match: {
-        lecturer: lecturerId,
-        slot_type: { $in: ["rescheduled", "cancelled", "ordinary"] },
-      },
-    },
-    {
-      $addFields: {
-        dateObject: {
-          $dateFromString: {
-            dateString: "$date",
-            format: "%Y-%m-%d",
-          },
-        },
-      },
-    },
-    {
-      $group: {
-        _id: {
-          month: { $month: "$dateObject" },
-          year: { $year: "$dateObject" },
-          slot_type: "$slot_type",
-        },
-        count: { $sum: 1 },
-      },
-    },
-    {
-      $group: {
-        _id: {
-          month: "$_id.month",
-          year: "$_id.year",
-        },
-        summary: {
-          $push: {
-            slot_type: "$_id.slot_type",
-            count: "$count",
-          },
-        },
-      },
-    },
-    {
-      $sort: { "_id.year": 1, "_id.month": 1 },
-    },
-  ]);
-
-  return summary;
-};
+const mongoose = require("mongoose");
 
 const getLecturerFullSummary = async (req, res) => {
   try {
     const lecturerId = req.user.id;
-    const summary = await getLecturerSummary(lecturerId);
+    const summary = await TimeTableSlot.aggregate([
+      {
+        $match: {
+          lecturer: new mongoose.Types.ObjectId(lecturerId),
+        },
+      },
+      {
+        $group: {
+          _id: "$slot_type",
+          NumberOfSessions: {
+            $sum: 1,
+          },
+        },
+      },
+    ]);
+    if (!summary) {
+      return res.status(200).json({ summary: [] });
+    }
 
     res.status(200).json({ summary });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
 };
+const getSemesterSummary = async (req, res) => {
+  try {
+    const summary = await TimeTableSlot.aggregate([
+      {
+        $group: {
+          _id: "$slot_type",
+          NumberOfSessions: {
+            $sum: 1,
+          },
+        },
+      },
+    ]);
+    if (!summary) {
+      return res.status(200).json({ summary: [] });
+    }
 
-module.exports = { getLecturerFullSummary };
+    return summary;
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+module.exports = { getLecturerFullSummary, getSemesterSummary };
--- routes/summaryRoutes.js
+++ routes/summaryRoutes.js
@@ -1,9 +1,14 @@
 const express = require("express");
-const { getLecturerFullSummary } = require("../controllers/summaryController");
+const {
+  getLecturerFullSummary,
+  getSemesterSummary,
+} = require("../controllers/summaryController");
 
 const router = express.Router();
 const isLecturer = require("../middlewares/verifyLecturer");
+const { isAdmin } = require("../middlewares/verifyAdmin");
 
 router.get("/lecturer-summary", isLecturer, getLecturerFullSummary);
+router.get("/semester-summary", isAdmin, getSemesterSummary);
 
 module.exports = router;
